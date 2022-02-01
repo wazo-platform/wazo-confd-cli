5d0c890e9b6c62ec30f6edb3a0fa0fd289d4520f
Pascal Cadotte Michaud <pcm@wazo.io>
2022-02-01 13:56:13 -0500

    endpoint: add the "add --template" subcommand
    this can be used to add a template to a SIP endpoint
    
    Usage:
      endpoint sip add --template <template-uuid> <endpoint_uuid>

--- README.md
+++ README.md
@@ -4,7 +4,16 @@ CLI tool to inspect and update wazo-confd resources
 
 ## Example usage
 
+### Listing resources
+
 ```shell
 export TOKEN=$(wazo-auth-cli token create --auth-username <username> --auth-password = <password>)
 wazo-confd-cli --token ${TOKEN} user list --tenant <tenant-uuid>
+```
+
+
+### Associating resources
+
+```shell
+wazo-confd-cli --token ${TOKEN} endpoint sip add --template <template-uuid> <endpoint-uuid>
 ```
--- wazo_confd_cli/commands/endpoint.py
+++ wazo_confd_cli/commands/endpoint.py
@@ -1,6 +1,7 @@
 # Copyright 2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+from cliff.command import Command
 from cliff.lister import Lister
 
 from ..helpers import ListBuildingMixin
@@ -27,6 +28,31 @@ REMOVED_ENDPOINT_COLUMNS = [
     'transport',
     'trunk',
 ]
+
+
+class EndpointSIPAdd(Command):
+    "Associate a group to another resource"
+
+    def get_parser(self, *args, **kwargs):
+        parser = super().get_parser(*args, **kwargs)
+        relation = parser.add_mutually_exclusive_group(required=True)
+        relation.add_argument(
+            '--template',
+            help='The UUID of the SIP endpoint template to add to this endpoint',
+        )
+        parser.add_argument('identifier', help='UUID of the endpoint')
+        return parser
+
+    def take_action(self, parsed_args):
+        endpoint_uuid = parsed_args.identifier
+
+        if parsed_args.template:
+            return self._add_template(endpoint_uuid, parsed_args.template)
+
+    def _add_template(self, endpoint_uuid, template_uuid):
+        endpoint = self.app.client.endpoints_sip.get(endpoint_uuid)
+        endpoint['templates'].append({'uuid': template_uuid})
+        self.app.client.endpoints_sip.update(endpoint)
 
 
 class EndpointSIPList(ListBuildingMixin, Lister):
